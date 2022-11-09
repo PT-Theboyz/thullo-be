d6951efbdb5a1c7843bab3a6f8452df3a9db4529
faisal <faisalfirdani01@gmail.com>
2022-11-10 00:11:32 +0700

    Create board and board user migration

---
+++ app/Http/Controllers/Api/BoardController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Board;
+use Illuminate\Http\Request;
+
+class BoardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validateBoard = Validator::make($request->all(), 
+        [
+            'name' => 'required',
+            'cover' => 'required',
+        ]);
+
+        if($validateBoard->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'validation error',
+                'errors' => $validateBoard->errors()
+            ], 401);
+        }
+
+        $board = Board::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => "Post Created successfully!",
+            'data' => $board
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Board  $board
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Board $board)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Board  $board
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Board $board)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Board  $board
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Board $board)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Board  $board
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Board $board)
+    {
+        //
+    }
+}
