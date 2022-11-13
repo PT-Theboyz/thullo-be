01c16d6cd742415cbad68ad02c66f4a7a8f4ae2b
faisal <faisalfirdani01@gmail.com>
2022-11-13 08:25:50 +0700

    Add role in user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('role');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password'); 
             $table->rememberToken();
             $table->timestamps();
         });
